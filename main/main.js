f6c8e8a7679cf79e97933eef458bc13fcce0ce9e
youths <552099787@qq.com>
2017-05-24 21:18:12 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,10 +1,9 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-	console.log(`o) 最大值 = ${sequence.maxmum()}
-	console.log(`o) 元素数量 = ${sequence.total()}
-	console.log(`o) 平均值 = ${sequence.average()}
-`);
+    console.log(`o) 最小值 = ${sequence.minimum()}`);
+	console.log(`o) 最大值 = ${sequence.maxmum()}`);
+	console.log(`o) 元素数量 = ${sequence.total()}`);
+	console.log(`o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
