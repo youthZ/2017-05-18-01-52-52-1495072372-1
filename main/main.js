c091e1e6290fddea6707aadca204390792b5939c
youths <552099787@qq.com>
2017-05-24 21:13:59 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,18 +1,49 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
+	console.log(`o) 最大值 = ${sequence.maxmum()}
+	console.log(`o) 元素数量 = ${sequence.total()}
+	console.log(`o) 平均值 = ${sequence.average()}
 `);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+    this.input=input;
   }
 
   minimum() {
-    // Write your code here
+    var array=this.input;
+	var min= array[0];
+	for(var i = 1; i < array.length-1; i++){
+		if(min > array[i])
+			max=array[i];
+	} 
+	return min;
   }
+	
+	maxmum(){
+		var array=this.input;
+		var max= array[0];
+		for(var i = 1; i < array.length-1; i++){
+			if(max < array[i])
+				max=array[i];
+		} 
+		return max;
+	}
 
-  // Write your code here
+	total(){
+		return this.input.length;
+	}
+
+	average(){
+		var array=this.input;
+		var sum= 0;
+		var avg = 0;
+		for(var i = 0; i < array.length-1; i++){
+			sum+=array[i];
+		} 
+		avg = Math.round((parseFloat(sum) / array.length)*100)*100;
+		return avg;
+	}
 }
